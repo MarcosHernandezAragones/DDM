bc7c7b5bd14fb2d1bd27b82b28e072d6b021caad
Marcos <marcoshernandezaraones@gmail.com>
2021-11-04 14:27:16 +0100

    falta funcionalidad

--- reto/iniciarSesion.php
+++ reto/iniciarSesion.php
@@ -18,8 +18,8 @@ $conexion=conectarBD();
 $user=$_POST['user'];
 $pass=$_POST['pass'];
 
-echo "Usuario(post): ".$user."<br>";
-echo "Contraseña(post): ".$pass."<br>";
+//echo "Usuario(post): ".$user."<br>";
+//echo "Contraseña(post): ".$pass."<br>";
 
 //preparamos la query
 $query = "SELECT * FROM usuario WHERE nombre=\"$user\" AND ".'password'."=\"$pass\"";
@@ -33,7 +33,7 @@ $nfilas=$consulta->rowCount();
 //Si solo devuelve una fila, es que el usuario existe
 if ($nfilas==1) {
     //hacemos una nueva query para comprobar si el usuario pertenece a docente(si no lo hace, es que es un alumno)
-    $query = "SELECT usuario_idUsuario, rol_idRol FROM docente, usuario WHERE usuario.idUsuario=usuario_idUsuario AND usuario.nombre=\"$user\"";
+    $query = "SELECT docente.usuario_idUsuario as idUsuario,centro.nombre as nombreCentro, usuario.nombre as nombre,docente.rol_idRol as rol FROM centro,docente, usuario WHERE usuario.idUsuario=usuario_idUsuario AND usuario.nombre=\"$user\" and docente.centro_idCentro=centro.idCentro";
 
     $consultaProf = $conexion->prepare($query);
     $consultaProf->execute();
@@ -42,8 +42,10 @@ if ($nfilas==1) {
     if ($nfilas==1) {
         //Si el usuario es un profesor entramos aqui
         $fila = $consultaProf->fetch();
-        $_SESSION['user']=$fila->usuario_idUsuario;
-        $_SESSION['rol']=$fila->rol_idRol;
+        $_SESSION['user']=$fila->idUsuario;
+        $_SESSION['nombre']=$fila->nombre;
+        $_SESSION['centro']=$fila->nombreCentro;
+        $_SESSION['rol']=$fila->rol;
         header("refresh:0;url=profesor.php");
         //REDIRECCIONAR A PROFESOR
     }else{
--- reto/profesor.php
+++ reto/profesor.php
@@ -1,4 +1,7 @@
-<?php session_start(); ?>
+<?php session_start(); 
+    $nombre=$_SESSION['nombre'];
+    $centro=$_SESSION['centro'];
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,14 +9,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo_prof.css">
+
     <title>Profesor</title>
 </head>
 <body>
+
     <section>
         <img class="gob" src="" alt="">
         <img class="centro" src="" alt="">
         <img class="abajo" src="logo_login.png" alt="" srcset="">
+        <p class="usuario_contro"><?php echo $nombre?></p>
+        <p class="nombre_centro"><?php echo $centro?></p>
+        
     </section>
     
+    <main>
+        <h1>SuperAdmin</h1>
+        <button class="propGrupos" onclick="redirigir('#')">Proponer Grupos</button>
+        <button class="verGrupos" onclick="redirigir('#')">Ver Grupos</button>
+        <button class="verAlumno" onclick="redirigir('#')">Ver Alumnos</button>
+        <button class="verClases" onclick="redirigir('#')">Ver Clases</button>
+        <button class="adminProf" onclick="redirigir('#')">Administrar Profesores</button>
+        <button class="salir" onclick="salir('login_reto.php')">Salir</button>
+    </main>
+    <script type="text/javascript" src="funciones.js"></script>
 </body>
 </html>
